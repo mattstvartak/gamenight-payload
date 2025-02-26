0754a819cc3e70f7c545843fb8b6d2aaa46cdb35
Matt Stvartak <hello@mattstvartak.com>
2025-02-26 11:24:26 -0500

    Update global for postgres

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -44,7 +44,7 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || "",
+      connectionString: process.env.POSTGRES_URL || "",
     },
   }),
   sharp,
