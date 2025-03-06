1198f55cbd7362860fe6478025505144ae4b6bed
Matt Stvartak <hello@mattstvartak.com>
2025-03-05 22:44:53 -0500

    removed migrations folder

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250306_010210 from './20250306_010210';
-
-export const migrations = [
-  {
-    up: migration_20250306_010210.up,
-    down: migration_20250306_010210.down,
-    name: '20250306_010210'
-  },
-];
